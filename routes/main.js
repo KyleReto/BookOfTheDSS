9b7fc84882f193e16c11094b30129a6056a8236b
Kyle Reto <kreto@uncc.edu>
2021-07-06 22:47:01 -0400

    Added the quote layout statically.

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 var router = express.Router();
 
+router.get('/quote', function(req, res){
+  res.render('quote');
+})
+
 router.get('/*', function(req, res){
   res.render('index');
 });
