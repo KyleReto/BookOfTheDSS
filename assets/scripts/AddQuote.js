68daf096ccc3526cabde1dcbd34d13ae6b024b2d
Kyle Reto <kreto@uncc.edu>
2021-07-11 05:12:34 -0400

    Refactored addquote route, allowing high #s of messages

--- assets/scripts/AddQuote.js
+++ assets/scripts/AddQuote.js
@@ -1,18 +1,14 @@
-// Starts at 0, plus the first message that's placed by default.
-let i = 1;
 document.getElementById('addMessageBtn').addEventListener('click', addMessage);
 // Dynamically add new message submissions to the page
 function addMessage(){
 	let container = document.getElementById('formContainer');
 	// Create the new inputs to add a new message
-	addInput(container, 'Sender: ', 'sender', i);
-	addInput(container, 'Message: ', 'message', i);
-	addInput(container, 'Embed File Name: ', 'embed', i);
+	addInput(container, 'Sender: ', 'sender');
+	addInput(container, 'Message: ', 'message');
+	addInput(container, 'Embed File Name: ', 'embed');
 	// Create br
 	let brElement = document.createElement('br');
 	container.appendChild(brElement);
-	// i iterates for each new message added.
-	i++;
 }
 
 // Add an input to a given form.
@@ -20,17 +16,17 @@ function addMessage(){
 // labelText: The visible text on the label
 // name: The name to set for the input
 // i: Concatenated onto the name parameter to give the final name.
-function addInput(container, labelText, name, i){
+function addInput(container, labelText, name){
 	// Create label
 	let labelElement = document.createElement('label');
 	let textNode = document.createTextNode(labelText);
 	labelElement.appendChild(textNode);
-	labelElement.setAttribute('for', name + i);
+	labelElement.setAttribute('for', name);
 	container.appendChild(labelElement);
 	// Create input
 	let inputElement = document.createElement('input');
 	inputElement.setAttribute('type', 'text');
-	inputElement.setAttribute('name', name + i);
+	inputElement.setAttribute('name', name);
 	container.appendChild(inputElement);
 	// Create br
 	let brElement = document.createElement('br');
--- routes/main.js
+++ routes/main.js
@@ -7,31 +7,20 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 let router = Express.Router();
 
-router.post('/newquote', urlencodedParser, async function(req, res){
+router.post('/addquote', urlencodedParser, async function(req, res){
 	// TODO: Add a password system, so that only authorized users can add quotes.
-	// This form is for internal use only. I'd love to make it accept variable amounts of messages, but this approach doesn't really allow for it, and I can't justify refactoring for something that only I will use.
-	console.log(req.body);
-	let message1 = new Message(req.body.sender1, req.body.message1, req.body.embed1);
-	let message2 = new Message(req.body.sender2, req.body.message2, req.body.embed2);
-	let message3 = new Message(req.body.sender3, req.body.message3, req.body.embed3);
-	let message4 = new Message(req.body.sender4, req.body.message4, req.body.embed4);
-	let message5 = new Message(req.body.sender5, req.body.message5, req.body.embed5);
-	let message6 = new Message(req.body.sender6, req.body.message6, req.body.embed6);
-	let message7 = new Message(req.body.sender7, req.body.message7, req.body.embed7);
-	let message8 = new Message(req.body.sender8, req.body.message8, req.body.embed8);
-	let temp = [message1, message2, message3, message4, message5, message6, message7, message8];
 	let messages = [];
-	// Process input request before sending to DB
-	for (let i = 0; i < temp.length; i++){
-		// Remove the message if the sender is empty
-		if (temp[i].sender != ""){
-			let message = temp[i];
-			// Null the embed if empty
-			if (message.embedPath == ""){
-				message.embedPath = null;
-			}
-			messages.push(message);
+	// For each message in the request
+	for (let i = 0; i < req.body.sender.length; i++){
+		// If there's no sender (message is empty/invalid), continue
+		if (req.body.sender[i] == '') continue
+
+		let message = new Message(req.body.sender[i], req.body.message[i], req.body.embed[i]);
+		// Null the embed if it's nonexistent
+		if (message.embedPath == ''){
+			message.embedPath = null;
 		}
+		messages.push(message)
 	}
 	let quote = new Quote(messages);
 	let id = await QuoteDB.addQuote(quote);
