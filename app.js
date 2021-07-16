8ed7070f22c9e7da3dc1d63968ed22f3a585a611
Kyle Reto <kreto@uncc.edu>
2021-07-16 14:45:11 -0400

    Fixed minor bugs, reworked app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const session = require('express-session');
+const path = require('path');
 const mainRouter = require('./routes/main.js');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
-app.use(express.static(`${__dirname}`));
-app.use('/assets', express.static('`${__dirname}/assets`'));
-app.use('/scripts', express.static('`${__dirname}/scripts`'));
+app.use('/assets/', express.static(path.join(__dirname, 'assets')));
+app.use('/favicon.ico', express.static('assets/favicon.ico'));
 app.use('/', mainRouter);
 
 app.listen(3000);
