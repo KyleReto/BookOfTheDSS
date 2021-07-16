ccf6f49b2748277c575b2734a99ed04fa328c90e
Kyle Reto <kreto@uncc.edu>
2021-07-15 22:51:18 -0400

    Revamped Submission Form

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const mainRouter = require('./routes/main.js');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets'));
+app.set('views', `${__dirname}/views`);
+app.use(express.static(`${__dirname}`));
+app.use('/assets', express.static('`${__dirname}/assets`'));
+app.use('/scripts', express.static('`${__dirname}/scripts`'));
+app.use('/favicon.ico', express.static('assets/favicon.ico'));
 app.use('/', mainRouter);
 
 app.listen(3000);
--- assets/scripts/AddQuote.js
+++ assets/scripts/AddQuote.js
@@ -1,34 +1,20 @@
-document.getElementById('addMessageBtn').addEventListener('click', addMessage);
+$("#addMessageBtn").click(addMessage);
+$("#avatarImage").change(reloadAvatar);
+$("")
+
 // Dynamically add new message submissions to the page
 function addMessage(){
-	let container = document.getElementById('formContainer');
-	// Create the new inputs to add a new message
-	addInput(container, 'Sender: ', 'sender');
-	addInput(container, 'Message: ', 'message');
-	addInput(container, 'Embed File Name: ', 'embed');
-	// Create br
-	let brElement = document.createElement('br');
-	container.appendChild(brElement);
+	let container = $('#formContainer');
+	// Create the new elements to allow an extra message
+	let avatarContainer = $("<div class=\"avatar\"></div>");
+	let avatarImg = $("<img src=\"/assets/avatars/Kyle.png\">");
+	avatarContainer.append(avatarImg);
+	container.append(avatarContainer);
+	// This is kind of absurd, I realize, but it is what it is.
+	let contentsDiv = $('<div class="contents"><h2 class="nameHeader"><div class="username"><input type="text" name="sender" placeholder="Kyle" class="username" ><br></div></h2><div class="messageText"><textarea rows="14" cols="10" wrap="soft" name="message" placeholder="Test Message" class="messageText"></textarea></div><div class="embed"><input type="text" name="embed" placeholder= "ImageEmbedFile.jpg"><br><img src="/assets/embeds/GodYourePathetic.jpg"></div></div><br>');
+	container.append(contentsDiv);
 }
 
-// Add an input to a given form.
-// container: The container to place the input in
-// labelText: The visible text on the label
-// name: The name to set for the input
-// i: Concatenated onto the name parameter to give the final name.
-function addInput(container, labelText, name){
-	// Create label
-	let labelElement = document.createElement('label');
-	let textNode = document.createTextNode(labelText);
-	labelElement.appendChild(textNode);
-	labelElement.setAttribute('for', name);
-	container.appendChild(labelElement);
-	// Create input
-	let inputElement = document.createElement('input');
-	inputElement.setAttribute('type', 'text');
-	inputElement.setAttribute('name', name);
-	container.appendChild(inputElement);
-	// Create br
-	let brElement = document.createElement('br');
-	container.appendChild(brElement);
+function reloadAvatar(){
+
 }
