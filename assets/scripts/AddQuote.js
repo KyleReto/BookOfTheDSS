7388d804798af9fe4c7de250b4919b0038d841d4
Kyle Reto <kreto@uncc.edu>
2021-07-15 23:49:40 -0400

    Added dynamic styling to submission form

--- assets/scripts/AddQuote.js
+++ assets/scripts/AddQuote.js
@@ -1,20 +1,34 @@
-$("#addMessageBtn").click(addMessage);
-$("#avatarImage").change(reloadAvatar);
-$("")
+$(document).ready(function(){
+	$("#addMessageBtn").click(addMessage);
+	$(".username").change(changeUsername);
+	$(".embedInput").change(reloadEmbed);
+})
 
 // Dynamically add new message submissions to the page
 function addMessage(){
 	let container = $('#formContainer');
 	// Create the new elements to allow an extra message
-	let avatarContainer = $("<div class=\"avatar\"></div>");
-	let avatarImg = $("<img src=\"/assets/avatars/Kyle.png\">");
-	avatarContainer.append(avatarImg);
-	container.append(avatarContainer);
 	// This is kind of absurd, I realize, but it is what it is.
-	let contentsDiv = $('<div class="contents"><h2 class="nameHeader"><div class="username"><input type="text" name="sender" placeholder="Kyle" class="username" ><br></div></h2><div class="messageText"><textarea rows="14" cols="10" wrap="soft" name="message" placeholder="Test Message" class="messageText"></textarea></div><div class="embed"><input type="text" name="embed" placeholder= "ImageEmbedFile.jpg"><br><img src="/assets/embeds/GodYourePathetic.jpg"></div></div><br>');
+	let contentsDiv = $('<div class="avatar"><img src="/assets/avatars/default.png"></div><div class="contents"><h2 class="nameHeader"><input type="text" name="sender" placeholder="IRL First Name" class="username"></h2><div class="messageText"><textarea rows="14" cols="10" wrap="soft" name="message" placeholder="Message text" class="messageText"></textarea></div><div class="embed"><input type="text" name="embed" placeholder= "ImageEmbedFile.jpg (optional)" class="embedInput"></div></div><br>');
 	container.append(contentsDiv);
 }
 
-function reloadAvatar(){
+// Update username color and avatar image on username change
+function changeUsername(){
+	let value = $(this).val();
+	$(this).css("color", "var(--username-" + value + ")");
+	// mildly cursed tree traversal
+	let avatar = $(this).parent().parent().prev().children();
+	avatar.attr("src", "/assets/avatars/" + value + ".png");
+}
 
+function reloadEmbed(){
+	let value = $(this).val();
+	// Remove the old embed, if it existed
+	$(this).next($("img")).remove();
+	// If there's an image to add
+	if (value != ''){
+		// Add said image to the DOM
+		$(this).parent().append($("<img src=\"/assets/embeds/" + value + "\">"))
+	}
 }
