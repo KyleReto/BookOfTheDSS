bc1ff994b2f871ca14fe3d86dae03e4c99ffb40f
KyleReto <40499747+KyleReto@users.noreply.github.com>
2021-10-08 02:11:38 -0400

    Improved avatar fetching for security and efficacy

--- config.js
+++ config.js
@@ -1,7 +1,31 @@
 
 let config = {};
 
-config.adminPass = "";
-config.listenPort = "10032";
+config.adminPass = "CXtM26yfbjUmyRL";
+config.listenPort = "3000";
+
+config.avatars = {
+    'Abi': 'Abi.png',
+    'Bronson': 'Bronson.png',
+    'Chris': 'Chris.png',
+    'Claire': 'Claire.png',
+    'David': 'David.png',
+    'Hiep': 'Hiep.png',
+    'I\'Mon': 'I\'Mon.png',
+    'Jack': 'Jack.png',
+    'Jakob': 'Jakob.png',
+    'James': 'James.png',
+    'Jayson': 'Jayson.png',
+    'Jon': 'Jon.png',
+    'Kyle': 'Kyle.png',
+    'Matt': 'Matt.png',
+    'Max': 'Max.png',
+    'Nicc': 'Nicc.png',
+    'Noah': 'Noah.png',
+    'Obt': 'Obt.png',
+    'Rodge': 'Rodge.png',
+    'Tahseen': 'Tahseen.png',
+    'Zach': 'Zach.png'
+};
 
 module.exports = config;
